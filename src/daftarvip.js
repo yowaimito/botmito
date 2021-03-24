5869b2be6bb21251c556f39e09f4e39988b8ad38
yowaimito <nazaco7870@gmail.com>
2021-03-24 02:43:58 -0300

    first commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5591984406769 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*𝕭𝖔𝖙 𝕯𝖆 𝕸𝖆𝖋𝖎𝖆🐊🚩 :*
+ 
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/554792091566*
+║
+╚═〘  𝕭𝖔𝖙 𝕯𝖆 𝕸𝖆𝖋𝖎𝖆  〙
+`
+}
+exports.iklan = iklan
